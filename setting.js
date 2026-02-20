4ecd913f8cc40d4fb2349c3024538ffb1e6e8548
adamsaputara807-sudo <adamsaputara807@gmail.com>
2026-02-20 17:19:18 +0700

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,5 @@
+
+global.owner = ["6285168610018"] // ubah jadi nomor mu
+global.name = "someone"
+global.frch = ["c97d9437c6b686e051e7bb4adc59e68a6c34fee821ef0ba859a54950ad344bf4"// Dapatkan apikey di https://asitha.top
+]
